5b093100bbf4c899aaf959922a623036924c5c58
hahnbi <hahnbi.sun@gmail.com>
2018-08-03 16:11:16 -0700

    Add CalendarDateControl

---
+++ src/components/dashboard/calendar_date_control.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class CalendarDateControl extends React.Component {
+  public render() {
+    return (
+      <div>
+        <div>Month</div>
+        <div>Year</div>
+      </div>
+    )
+  }
+}
+
+export default CalendarDateControl;
--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
+import CalendarDateControl from 'src/components/dashboard/calendar_date_control';
 import CalendarView from 'src/components/dashboard/calendar_view';
 
 class Dashboard extends React.Component {
   public render() {
     return (
       <div className="container-fluid">
-        <div>Dashboard</div>
+        <CalendarDateControl />
         <CalendarView />
       </div>
     )
