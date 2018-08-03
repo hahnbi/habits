eda45dce506b057cd4dc15c251c77206b8da05a1
hahnbi <hahnbi.sun@gmail.com>
2018-08-03 09:21:14 -0700

    Add sample user service file

---
+++ src/services/user_service.ts
@@ -0,0 +1,18 @@
+//tslint:disable
+import db from 'src/app_init/firebase';
+
+const UserService = {
+  addUser() {
+    db.collection('users').add({
+      firstName: 'Hahnbi',
+      lastName: 'Sun',
+    }).then((docRef) => {
+      console.log("Document written with ID: ", docRef.id);
+  })
+  .catch((error) => {
+      console.error("Error adding document: ", error);
+  });
+  }
+}
+
+export default UserService;
